9624bc07efd9ef40454cc68918612811328701b2
elaami <melika.melika8@gmail.com>
2025-04-23 14:32:31 +0330

    button component

---
+++ src/app/components/button.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Button() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+
+    <div className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none'>button</div>
+    </div>
+  )
+}
