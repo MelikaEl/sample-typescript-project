1296335d5daab19dc763d953aa41b524117608ff
elaami <melika.melika8@gmail.com>
2025-04-26 11:01:55 +0330

    style object

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,10 +1,136 @@
-import React from 'react'
+import React from "react";
 
-export default function Button() {
+
+
+type ButtonProps = {
+  style:{
+  backgroundColor: string;
+  fontSize: number;
+  textColor: string;
+}};
+
+export default function Button({ style }: ButtonProps) { //style object
   return (
-    <div className='flex justify-center items-center h-screen'>
-
-    <div className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none'>button</div>
+    <div className="flex justify-center items-center h-screen">
+      <button
+        style={style}
+      >
+        Click me
+      </button>
     </div>
-  )
+  );
+}
+
+//part4
+
+{
+  /*
+  import React from "react";
+
+type Color = "red" | "blue" | "green" | "yellow" | "purple";
+
+type ButtonProps = {
+  backgroundColor: Color;
+  textColor: Color;
+  fontSize: number;
+  padding: [number, number, number, number]; // Tuple of 4 numbers
+};
+
+export default function Button(props: ButtonProps) {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button
+        style={{
+          backgroundColor: props.backgroundColor,
+          color: props.textColor,
+          fontSize: `${props.fontSize}px`,
+          padding: `${props.padding[0]}px ${props.padding[1]}px ${props.padding[2]}px ${props.padding[3]}px`,
+        }}
+        className="hover:bg-opacity-80 focus:ring-4 focus:ring-blue-300 rounded-lg focus:outline-none"
+      >
+        Click me
+      </button>
+    </div>
+  );
+}
+  */
+}
+
+//part3
+{
+  /*
+ import React from "react";
+
+type Color = "red" | "blue" | "green" | "yellow" | "purple";
+
+type ButtonProps = {
+  backgroundColor: Color;
+  textColor: Color;
+  fontSize: number;
+  // pillShape?: boolean;
+  padding: [number, number, number, number]; //tuple of 4 numbers
+  // padding: number[]; //array of numbers: by this way we can pass any number of padding values but just we want 4 values
+};
+
+export default function Button({
+ 
+  backgroundColor,
+  fontSize,
+  // pillShape,
+  textColor,
+  padding,
+}: ButtonProps) {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button style={{ backgroundColor:backgroundColor, color: textColor , fontSize:fontSize,padding:`${padding[0]}px ${padding[1]}px ${padding[2]}px ${padding[3]}px`}} >
+        Click me
+      </button>
+    </div>
+  );
+}
+  */
+}
+
+//part2
+{
+  /*
+  import React from "react";
+
+export default function Button({
+  backgroundColor,
+  fontSize
+}: {
+  backgroundColor: string;
+  fontSize: number;
+}) {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button style={{ backgroundColor:backgroundColor , fontSize:fontSize}}>
+        Click me
+      </button>
+    </div>
+  );
+}
+  */
+}
+
+//part1
+{
+  /*
+import React from "react";
+
+export default function Button(props: {
+  backgroundColor: string;
+  fontSize: number;
+}) {
+  const { backgroundColor, fontSize } = props;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button style={{ backgroundColor:backgroundColor , fontSize:fontSize}} >
+        Click me
+      </button>
+    </div>
+  );
+}
+    */
 }
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import Button from '../components/button'
+import React from "react";
+import Button from "../components/button";
 
 export default function page() {
   return (
-
-    <div> <Button/></div>
-   
-  )
+    <div>
+      {" "}
+      <Button
+        style={{
+          backgroundColor: "yellow",
+          fontSize: 40,
+          textColor: "white",
+        }}
+      />
+    </div>
+  );
 }
+
+
+
+//part4,part3
+{
+  /*
+import React from "react";
+import Button from "../components/button";
+
+export default function page() {
+  return (
+    <div>
+      {" "}
+      <Button
+        backgroundColor="yellow"
+        fontSize={30}
+        textColor="green"
+        padding={[5, 10, 20, 50]}
+      />
+    </div>
+  );
+}
+   */
+}
+
+
+//part1,part2
+{/*
+  import React from "react";
+import Button from "../components/button";
+
+export default function page() {
+  return (
+    <div>
+      {" "}
+      <Button
+        backgroundColor="yellow"
+        fontSize={30}
+      />
+    </div>
+  );
+}
+  */}
